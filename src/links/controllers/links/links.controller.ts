9fcdcf0d931ea796b2443ce4849619286337c249
Ahmed F Gadir <agadir@sgintl.com>
2022-12-26 13:50:30 -0500

    Updated API to have HIT

--- src/links/controllers/links/links.controller.ts
+++ src/links/controllers/links/links.controller.ts
@@ -1,35 +1,41 @@
-
 import {
-    Body,
-    Controller,
-    Get,
-    Param,
-    ParseIntPipe,
-    Post,
-    UsePipes,
-    ValidationPipe,
-    } from '@nestjs/common';
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 import { CreateLinkDto } from 'src/links/links.dtos';
 import { LinksService } from 'src/links/services/links/links.service';
 
 @Controller('links')
 export class LinksController {
-    constructor(private readonly linkService: LinksService) {}
-    
-    @Get()
-    getLinks() {
-      return this.linkService.getLinks();
-    }
-    
-    @Get('id/:id')
-    findLinksById(@Param('id', ParseIntPipe) id: number) {
-      return this.linkService.findLinksById(id);
-    }
-    
-    @Post()
-    @UsePipes(ValidationPipe)
-    createLinks(@Body() createLinkDto: CreateLinkDto) {
-      return this.linkService.createLink(createLinkDto);
-    }
+  constructor(private readonly linkService: LinksService) {}
+
+  @Get()
+  getLinks() {
+    return this.linkService.getLinks();
   }
+
+  @Get('id/:id')
+  findLinksById(@Param('id', ParseIntPipe) id: number) {
+    return this.linkService.findLinksById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createLinks(@Body() createLinkDto: CreateLinkDto) {
+    return this.linkService.createLink(createLinkDto);
+  }
+
+  @Post('hit/:id')
+  //@UsePipes(ValidationPipe)
+  linkhits(@Param('id', ParseIntPipe) id: number) {
+    var hit = this.linkService.findLinksById(id);
+    return hit;
+  }
+}
--- src/typeorm/link.entity.ts
+++ src/typeorm/link.entity.ts
@@ -1,30 +1,34 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class link{
-    @PrimaryGeneratedColumn({
-        type:'bigint',
-        name:'id'
-    })
-    id: number;
+export class link {
+  @PrimaryGeneratedColumn({
+    type: 'bigint',
+    name: 'id',
+  })
+  id: number;
 
+  @Column({
+    nullable: false,
+    default: 0,
+  })
+  sort: number;
 
-    @Column({
-        nullable:false,
-        default:0
-    })
-    sort: number;
+  @Column({
+    nullable: true,
+    default: '',
+  })
+  name: string;
 
-    @Column({
-        nullable: true,
-        default:''
-    })
-    name: string;
+  @Column({
+    nullable: true,
+    default: '',
+  })
+  url: string;
 
-    @Column({
-        nullable:true,
-        default:''
-    })
-    url: string
-
+  @Column({
+    nullable: false,
+    default: 0,
+  })
+  hit: number;
 }
